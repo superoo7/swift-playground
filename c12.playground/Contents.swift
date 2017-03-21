3af3fe7d544fb89898e7bb9e8251335c55ce504b
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-03-22 00:26:51 +0800

    saved c12

--- c12.playground/Contents.swift
+++ c12.playground/Contents.swift
@@ -11,4 +11,14 @@ func printGreetings(){
 // calling the func
 printGreetings()
 
+//division func
+func divisionDescriptionFor(numerator: Double, denominator: Double) {
+    print("\(numerator) divided by \(denominator) equals \(numerator / denominator)")
+}
+divisionDescriptionFor(numerator: 9.0, denominator: 3.0)
 
+// Params name with (:to) make it more readable
+func printPersonalGreeting(to name: String) {
+    print("Hello \(name), welcome to your playground.")
+}
+printPersonalGreeting(to: "Johnson")
