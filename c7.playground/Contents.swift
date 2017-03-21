0a5541e7b49dbd0ff0a0a6db04ec4eb05cc17000
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-03-21 12:13:25 +0800

    finished c7 left challenges

--- c7.playground/Contents.swift
+++ c7.playground/Contents.swift
@@ -1,5 +1,25 @@
 //: Playground - noun: a place where people can play
 
+// c7
 import Cocoa
 
-var str = "Hello, playground"
+let test: String = "hello"
+var array1: [Character] = []
+// .characters split string into characters
+for c: Character in test.characters {
+    array1.append(c)
+}
+print(array1)
+
+// Unicode
+print("\u{0301}")
+
+let testcode = "\u{0061}\u{0301}"
+
+for scalar in testcode.unicodeScalars {
+    print("\(scalar.value) ")
+}
+
+// Bronze Challenge
+
+// Silver Challenge
