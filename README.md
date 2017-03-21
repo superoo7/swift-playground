49eac22286a59fdaa80c281e7cfe6f0087313fca
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-03-21 17:45:42 +0800

    finished c9

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Learning Swift
 ## Programming language with playground in xcode 8
 
-### created by [superoo7](http://superoo7/com)
+### created by [superoo7](http://superoo7.com)
 
 playground for macOS with xCode 8
 
--- c9.playground/Contents.swift
+++ c9.playground/Contents.swift
@@ -4,3 +4,31 @@
 
 import Cocoa
 
+//creating an array
+//method 1
+var bucketlist1: Array<String>?
+//method2
+var bucketlist2: [String] = []
+
+// Array are 0 indexed
+//method3
+var bucketlist3 = ["Climb Mt. Everest","Talk to Warren Buffet", "Invented a new language", "Talk to Steve Job","invent an invisible coat"]
+bucketlist3.append("Create a robot")
+bucketlist3.remove(at: 3)
+bucketlist3[0] = "Climb Mt. Kinabalu"
+print(bucketlist3.count)
+print(bucketlist3[0...2])
+
+//method4
+var items = [1,2,3,4,5,6,7,8,9]
+
+for i in items {
+    bucketlist2.append("\(i)")
+}
+
+print(bucketlist2)
+
+//Challenges
+
+
+
