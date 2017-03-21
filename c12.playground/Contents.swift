fc6e905c96cdc0d7da67346bf483746851a0ecf9
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-03-22 00:14:04 +0800

    added c12 and added some content

---
+++ c12.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+// c12
+// function
+
+import Cocoa
+
+// basic func
+func printGreetings(){
+    print("Greetings!")
+}
+// calling the func
+printGreetings()
+
+
